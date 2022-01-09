4a198f1655692032da379bda826b4fc8cbc3cfe3
James McNeil <mcneil_james@hotmail.com>
2022-01-09 20:10:05 +0000

    created Navbar components and css module

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+// Required react components
+import styles from './Navbar.module.css'
+
+// styles
+import { Link } from 'react-router-dom'
+
+// app components/pages
+import Home from '../pages/home/Home'
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>myMoney</li>
+        <li>
+          <Link to='/login'>Login</Link>
+          <Link to='/signup'>SignUp</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
