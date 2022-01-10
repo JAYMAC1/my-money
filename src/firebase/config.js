32907ffc26d10084fa984f1d182e08870b246e28
James McNeil <mcneil_james@hotmail.com>
2022-01-10 12:49:17 +0000

    created firebase config file and init projectFirestore to use firebase/firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD6UR5LrDwY9_EoEKHExckGsWMIslVthS8',
+  authDomain: 'mymoney-e1cba.firebaseapp.com',
+  projectId: 'mymoney-e1cba',
+  storageBucket: 'mymoney-e1cba.appspot.com',
+  messagingSenderId: '413180075714',
+  appId: '1:413180075714:web:d618f593fe9025cb1da55b',
+}
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
