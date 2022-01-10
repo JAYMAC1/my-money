4fba9fbec29426a283c8ccec343e3be045410203
James McNeil <mcneil_james@hotmail.com>
2022-01-10 13:19:32 +0000

    imported useSignup added

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,15 +1,19 @@
-// styles
 import { useState } from 'react'
+import { useSignup } from '../../hooks/useSignup'
+
+// styles
 import styles from './Signup.module.css'
 
 const Signup = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [displayName, setDisplayName] = useState('')
+  const { error, isPending, signup } = useSignup()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password, displayName)
+
+    signup(email, password, displayName)
   }
   return (
     <form onSubmit={handleSubmit} className={styles['signup-form']}>
@@ -44,7 +48,13 @@ const Signup = () => {
           value={displayName}
         />
       </label>
-      <button className='btn'>Signup</button>
+      {!isPending && <button className='btn'>Signup</button>}
+      {isPending && (
+        <button className='btn' disabled>
+          loading
+        </button>
+      )}
+      {error && <p>{error} </p>}
     </form>
   )
 }
