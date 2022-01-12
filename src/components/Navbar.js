01ecbbd74bef9677afd6459bc9932befc9e6524f
James McNeil <mcneil_james@hotmail.com>
2022-01-12 13:59:20 +0000

    imported and added useLogout hook with the Navbar. Added Logout button with onclick to action

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 // Required react components
-import styles from './Navbar.module.css'
-
-// styles
 import { Link } from 'react-router-dom'
 
+// styles
+import styles from './Navbar.module.css'
+
 // app components/pages
+import useLogout from '../hooks/useLogout'
 
 const Navbar = () => {
+  const { logout } = useLogout()
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -14,6 +16,11 @@ const Navbar = () => {
         <li>
           <Link to='/login'>Login</Link>
           <Link to='/signup'>SignUp</Link>
+        </li>
+        <li>
+          <button className='btn' onClick={logout}>
+            Logout
+          </button>
         </li>
       </ul>
     </nav>
