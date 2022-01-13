be9bde930669be4d4919ac75ea688d7245884fa6
James McNeil <mcneil_james@hotmail.com>
2022-01-13 13:23:18 +0000

    tidy up of code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,23 +5,37 @@ import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
 
 // app components/pages
+import { useAuthContext } from '../hooks/useAuthContext'
 import useLogout from '../hooks/useLogout'
 
 const Navbar = () => {
   const { logout } = useLogout()
+  const { user } = useAuthContext()
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
-        <li>
-          <Link to='/login'>Login</Link>
-          <Link to='/signup'>SignUp</Link>
-        </li>
-        <li>
-          <button className='btn' onClick={logout}>
-            Logout
-          </button>
-        </li>
+
+        {!user && (
+          <>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>SignUp</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>Hello, {user.displayName}</li>
+            <li>
+              <button className='btn' onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   )
