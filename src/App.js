e7beb8570e49d04790b4300424cffc7b66ef8cf4
James McNeil <mcneil_james@hotmail.com>
2022-01-13 13:34:27 +0000

    imported useAuthContext to validate and show content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // required react components
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { useAuthContext } from './hooks/useAuthContext'
 
 // App components/pages
 import Home from './pages/home/Home'
@@ -8,22 +9,25 @@ import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 
 function App() {
+  const { authIsReady } = useAuthContext()
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/signup'>
-            <Signup />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   )
 }
