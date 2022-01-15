e28dfbee36d87e3022330d169f1a52a79e4c0dc6
James McNeil <mcneil_james@hotmail.com>
2022-01-15 15:36:40 +0000

    Changed title from myMoney to myMoneyApp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className={styles.title}>myMoney</li>
+        <li className={styles.title}>myMoneyApp</li>
 
         {!user && (
           <>
