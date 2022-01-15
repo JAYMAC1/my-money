8edce5da5c2c9b5344a1ec062157d7d68e6ad3ab
James McNeil <mcneil_james@hotmail.com>
2022-01-15 15:05:49 +0000

    used useFirestore hook to action deleteDocument function on listed transactions

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,11 +1,15 @@
 import styles from './Home.module.css'
+import { useFirestore } from '../../hooks/useFirestore'
 const TransactionList = ({ transactions }) => {
+  const { deleteDocument, response } = useFirestore('transactions')
+  console.log(response)
   return (
     <ul className={styles.transactions}>
       {transactions.map((transaction) => (
         <li key={transaction.id}>
           <p className={styles.name}>{transaction.name}</p>
           <p className={styles.amount}>£{transaction.amount}</p>
+          <button onClick={() => deleteDocument(transaction.id)}>x</button>
         </li>
       ))}
     </ul>
